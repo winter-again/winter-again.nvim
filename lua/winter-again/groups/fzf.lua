f7a86990b5b4954cdcdbf11187591097c3dd39a1
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-08-27 23:01:56 -0400

    fzf

--- lua/winter-again/groups/fzf.lua
+++ lua/winter-again/groups/fzf.lua
@@ -2,14 +2,13 @@ local colors = require("winter-again.colors").colors
 
 local M = {
     FzfLuaNormal = { fg = colors.fg, bg = colors.bg_float },
-    FzfLuaFzfPrompt = { fg = colors.purple, bg = colors.bg_float },
+    FzfLuaHeaderBind = { fg = colors.green, bg = colors.bg_float },
     FzfLuaHeaderText = { fg = colors.green, bg = colors.bg_float },
-    FzfLuaHeaderBind = { fg = colors.blue, bg = colors.bg_float },
-    FzfLuaCursorLine = { link = "CursorLine" },
-    FzfLuaCursorLineNr = { link = "CursorLineNr" },
 
     -- NOTE: these don't apply to rg output in live_grep
     FzfLuaPathLineNr = { fg = colors.green },
+    FzfLuaBufFlagCur = { fg = colors.purple },
+    FzfLuaBufFlagAlt = { fg = colors.blue },
     FzfLuaPathColNr = { fg = colors.yellow },
 }
 
